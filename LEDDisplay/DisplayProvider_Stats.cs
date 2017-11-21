7f60e794aecb0334ccce84e872aba121892c9f4b
Nhat Nguyen <hugo6644493@gmail.com>
2017-11-21 22:24:51 +0200

    Added RAM and CPU usage display (DisplayProvided_Stats)

---
+++ LEDDisplay/DisplayProvider_Stats.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace LEDDisplay
+{
+    class DisplayProvider_Stats : DisplayProvider
+    {
+        public static new string Title
+        {
+            get
+            {
+                return "Computer Stats";
+            }
+        }
+
+        public override void Draw(ref ScrollingFrameBuffer buffer)
+        {
+            PerformanceCounter perfMemCount = new PerformanceCounter("Memory", "Available MBytes");
+
+            string ramString = String.Format("{0}MB RAM", (int)perfMemCount.NextValue());
+
+            buffer = new ScrollingFrameBuffer(LEDFont.stringWidth(ramString));
+            buffer.ViewOffset = -21;
+            LEDFont.DrawString(ref buffer, 0, ramString);
+        }
+
+        public override void OnClick(object sender, EventArgs e)
+        {
+            Draw(ref LEDDisplayApp.frameBuffer);
+            LEDDisplayApp.HandleClick(Title);
+        }
+    }
+}
--- LEDDisplay/Properties/Settings.Designer.cs
+++ LEDDisplay/Properties/Settings.Designer.cs
@@ -143,5 +143,17 @@ namespace LEDDisplay.Properties {
                 this["GMailFormat"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool EnableStats {
+            get {
+                return ((bool)(this["EnableStats"]));
+            }
+            set {
+                this["EnableStats"] = value;
+            }
+        }
     }
 }
